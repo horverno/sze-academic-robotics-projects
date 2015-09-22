340af94b17d9165fb1ece27bf4fad89c80ecf2d6
Erno Horvath <horverno@gmail.com>
2015-09-22 11:00:33 +0200

    Vrep Simple test updated with drive commands
    postion update from a thread,
    forward, backward, turn drive command (vecoity reference) added

--- VrepCsharpInteroperation/VrepSimpleTest/MainWindow.xaml.cs
+++ VrepCsharpInteroperation/VrepSimpleTest/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
 using remoteApiNETWrapper;
 
 namespace VrepSimpleTest
@@ -19,7 +22,7 @@ namespace VrepSimpleTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         /// <summary>
         /// True - connected to V-REP; False - not connected
@@ -28,12 +31,39 @@ namespace VrepSimpleTest
         /// <summary>
         /// V-REP connection ID
         /// </summary>
-        int _clientID = -1; 
+        int _clientID = -1;
+        bool _updatePos = false;
+        float[] _pos = new float[3];
+        Thread _up;
+        private string _posString = "position";
+        /// <summary>
+        /// The handles for the robots
+        /// </summary>
+        int _handleNeo0, _handleNeo1;
+        /// <summary>
+        /// The handles for the motors
+        /// </summary>
+        int _handleLeftMotor0, _handleRightMotor0;
+
         public MainWindow()
         {
             InitializeComponent();
             stackControls.Visibility = Visibility.Hidden;
         }
+
+
+        public string PositionString
+        {
+            get { return _posString; }
+            set
+            {
+                _posString = value;
+                OnPropertyChanged("PositionString");
+            }
+        }
+
+
+
         /// <summary>
         /// Connect or disconnect from V-REP
         /// </summary>
@@ -51,18 +81,29 @@ namespace VrepSimpleTest
                 {
                     MessageBox.Show("remoteApi.dll missing");
                 }
-                
+
                 if (_clientID != -1) // Successfully connected to V-REP
                 {
-                    System.Diagnostics.Debug.WriteLine("Connected to V-REP");
+                    Debug.WriteLine("Connected to V-REP");
                     buttonConnect.Background = Brushes.LightGreen;
                     _connected = true;
                     stackControls.Visibility = Visibility.Visible;
                     buttonConnect.Content = "Disconnect";
+                    VREPWrapper.simxGetObjectHandle(_clientID, "neobotix#0", out _handleNeo0, simx_opmode.oneshot_wait);
+                    Debug.WriteLine("Handle neobotix#0: " + _handleNeo0);
+                    VREPWrapper.simxGetObjectHandle(_clientID, "neobotix#1", out _handleNeo1, simx_opmode.oneshot_wait);
+                    Debug.WriteLine("Handle neobotix#1: " + _handleNeo1);
+                    VREPWrapper.simxGetObjectHandle(_clientID, "wheel_left#0", out _handleLeftMotor0, simx_opmode.oneshot_wait);
+                    Debug.WriteLine("Handle left motor #0: " + _handleLeftMotor0);
+                    VREPWrapper.simxGetObjectHandle(_clientID, "wheel_right#0", out _handleRightMotor0, simx_opmode.oneshot_wait);
+                    Debug.WriteLine("Handle right motor #0: " + _handleRightMotor0);
+                    if (_handleNeo0 == 0 && _handleNeo1 == 0)
+                        MessageBox.Show("Connected, but no neobotix robot found");
+
                 }
                 else // Connection trial failed
                 {
-                    System.Diagnostics.Debug.WriteLine("Error connecting to V-REP");
+                    Debug.WriteLine("Error connecting to V-REP");
                     MessageBox.Show("Error connecting to V-REP :(");
                     buttonConnect.Background = Brushes.OrangeRed;
                     _connected = false;
@@ -76,26 +117,116 @@ namespace VrepSimpleTest
                 buttonConnect.Content = "Reconnect";
                 _connected = false;
                 stackControls.Visibility = Visibility.Hidden;
-                System.Diagnostics.Debug.WriteLine("Disconnected from V-REP");
+                Debug.WriteLine("Disconnected from V-REP");
+                _updatePos = false;
             }
         }
         private void buttonGetPos_Click(object sender, RoutedEventArgs e)
         {
-            int handleNeo0, handleNeo1; // The handles for the robots
-            float[] pos = new float[3]; // The position of the #0 robot relative to the #1 robot
-            VREPWrapper.simxGetObjectHandle(_clientID, "neobotix#0", out handleNeo0, simx_opmode.oneshot_wait);
-            System.Diagnostics.Debug.WriteLine("Handle neobotix#0: " + handleNeo0);
-            VREPWrapper.simxGetObjectHandle(_clientID, "neobotix#1", out handleNeo1, simx_opmode.oneshot_wait);
-            System.Diagnostics.Debug.WriteLine("Handle neobotix#1: " + handleNeo1);
-            VREPWrapper.simxGetObjectPosition(_clientID, handleNeo0, handleNeo1, pos, simx_opmode.oneshot_wait);
-            System.Diagnostics.Debug.WriteLine("x:{0:F4} y:{1:F4} z:{2:F4}", pos[2], pos[1], pos[0]);
-            txtInfo.Text = String.Format("x:{0:F4} y:{1:F4} z:{2:F4}", pos[2], pos[1], pos[0]);
+            if (!_updatePos)
+            {
+                _up = new Thread(new ThreadStart(upd));
+                _up.Start();
+                buttonGetPos.Content = "Getting position...";
+            }
+            else
+            {
+                buttonGetPos.Content = "Get position";
+            }
+            _updatePos = !_updatePos;
         }
         private void buttonScan_Click(object sender, RoutedEventArgs e)
         {
-
+            //VREPWrapper.simxGetObjectPosition(_clientID, _handleNeo0, _handleNeo1, _pos, simx_opmode.oneshot_wait);
+            //Debug.WriteLine("x:{0:F4} y:{1:F4} z:{2:F4}", _pos[2], _pos[1], _pos[0]);
+            //txtInfo.Text = String.Format("x:{0:F4} y:{1:F4} z:{2:F4}", _pos[2], _pos[1], _pos[0]);
         }
 
+        public void upd()
+        {
+            int handleNeo0, handleNeo1; // The handles for the robots
+            float[] pos = new float[3]; // The position of the #0 robot relative to the #1 robot
+            while (_updatePos) // todo
+            {
+                VREPWrapper.simxGetObjectPosition(_clientID, _handleNeo0, _handleNeo1, _pos, simx_opmode.oneshot_wait);
+                PositionString = String.Format("x:{0:F4} y:{1:F4} z:{2:F4}", _pos[2], _pos[1], _pos[0]);
+                DrawSomething(); // todo
+                Thread.Sleep(400);
+            }
+        }
 
+        private void buttonFwd_Click(object sender, RoutedEventArgs e)
+        {
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleLeftMotor0, 10, simx_opmode.oneshot_wait);
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleRightMotor0, 10, simx_opmode.oneshot_wait);
+        }
+
+        private void buttonBck_Click(object sender, RoutedEventArgs e)
+        {
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleLeftMotor0, -10, simx_opmode.oneshot_wait);
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleRightMotor0, -10, simx_opmode.oneshot_wait);
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleLeftMotor0, 0, simx_opmode.oneshot_wait);
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleRightMotor0, 0, simx_opmode.oneshot_wait);
+        }
+
+        private void buttonRight_Click(object sender, RoutedEventArgs e)
+        {
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleLeftMotor0, 10, simx_opmode.oneshot_wait);
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleRightMotor0, -10, simx_opmode.oneshot_wait);
+        }
+
+        private void buttonLeft_Click(object sender, RoutedEventArgs e)
+        {
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleLeftMotor0, -10, simx_opmode.oneshot_wait);
+            VREPWrapper.simxSetJointTargetVelocity(_clientID, _handleRightMotor0, 10, simx_opmode.oneshot_wait);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            // todo
+            _updatePos = false;
+            try
+            {
+                if (_up.IsAlive)
+                    _up.Abort();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception occured");
+            }
+
+            if (_connected)
+                VREPWrapper.simxFinish(_clientID);
+        }
+
+        void DrawSomething()
+        {
+            //todo
+            //canvScan.Children.Add(new Line
+            //{
+            //    Stroke = System.Windows.Media.Brushes.DarkGray,
+            //    StrokeThickness = 2,
+            //    X1 = 150,
+            //    Y1 = 150,
+            //    X2 = (int)(160),
+            //    Y2 = (int)(160)
+            //});
+        }
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
     }
 }
