1334aabb5866fdfc5c87081e6f51f8cde71d7aaf
Erno Horvath <horverno@gmail.com>
2015-10-25 17:05:46 +0100

    Filtering out the robot"s own contour from laser scanner measurements

--- VrepMatlabInteroperation/VrepMatlabTest/robotMoveAndStopTest3.m
+++ VrepMatlabInteroperation/VrepMatlabTest/robotMoveAndStopTest3.m
@@ -15,7 +15,7 @@ end
 [rtn,neoStartPos] = vrep.simxGetObjectPosition(clientID, neoHandle0, origoHandle, vrep.simx_opmode_oneshot_wait);
 r = 0.125;
 turns = 0;
-target_position = 4.0; % 
+target_position = 1.0; % 
 omega = 2;
 time = target_position/(r*omega);
 target_angle = time * omega;
@@ -28,7 +28,7 @@ i = 0;
 pos = 0;
 prevPos = [0 0]; % contains the actual (2) and the previous position of the wheel (1) 
 pause(1);
-vrep.simxSetJointTargetVelocity(clientID, motorLeft, -2, vrep.simx_opmode_oneshot_wait);
+vrep.simxSetJointTargetVelocity(clientID, motorLeft, 2, vrep.simx_opmode_oneshot_wait);
 vrep.simxSetJointTargetVelocity(clientID, motorRight, 2, vrep.simx_opmode_oneshot_wait);
 fig2 = figure('Name', 'Mapping');
 thetaHistory = [0];
@@ -48,7 +48,6 @@ while turns*pi*2+pos < target_angle % the robot moves until it reaches the targe
     data = vrep.simxUnpackFloats(laser_scan);
     data = reshape(data,3,size(data,2)/3);
     outer_hull3 = data(:,end-684:end);
-    %outer_hull3 = outer_hull3 / 3.42; %
     [rtn,neoEndPos] = vrep.simxGetObjectPosition(clientID, neoHandle0, origoHandle, vrep.simx_opmode_oneshot_wait);
     [rtn,neoEndTheta] = vrep.simxGetObjectOrientation(clientID, neoHandle0, origoHandle, vrep.simx_opmode_oneshot_wait);
     if neoEndTheta(1) < 0
@@ -59,9 +58,18 @@ while turns*pi*2+pos < target_angle % the robot moves until it reaches the targe
     thetaHistory = [thetaHistory; neoEndTheta(2)];
     outer_hull3 = [outer_hull3(1,:) ; (outer_hull3(2,:) .* -1); (outer_hull3(3,:) - 0.17)]; % flip laser scanner data according Y, move down according Z
     thetaRotate = (2 * neoEndTheta(2) - pi);
-    oh3 = [cos(thetaRotate),-sin(thetaRotate),0;sin(thetaRotate),cos(thetaRotate),0;0,0,1]*outer_hull3;
-    oh3 = [oh3(1,:) + neoEndPos(1); oh3(2,:) + neoEndPos(2); oh3(3,:)];
-    plot(oh3(1,:),oh3(2,:), '*', 'Color', rand(1,3))
+    oh3_ = [;]; % filtering the own contour of the robot from the laser scanner measurement
+    for n = 1:size(outer_hull3, 2)
+        if ~(abs(outer_hull3(1,n)) < 0.3 & abs(outer_hull3(2,n)) < 0.3)
+            oh3_(1,n) = outer_hull3(1,n);
+            oh3_(2,n) = outer_hull3(2,n); 
+            oh3_(3,n) = outer_hull3(3,n);            
+        end
+    end
+    outer_hull3 = oh3_;
+    oh3 = [cos(thetaRotate),-sin(thetaRotate),0;sin(thetaRotate),cos(thetaRotate),0;0,0,1]*outer_hull3; % rotate laser scanner data (orientation)
+    oh3 = [oh3(1,:) + neoEndPos(1); oh3(2,:) + neoEndPos(2); oh3(3,:)]; % move laser scanner data (position)
+    plot(oh3(1,:),oh3(2,:), '.', 'Color', rand(1,3))
     drawnow %
     hold on
 end
@@ -70,7 +78,7 @@ vrep.simxSetJointTargetVelocity(clientID, motorLeft, 0, vrep.simx_opmode_oneshot
 vrep.simxSetJointTargetVelocity(clientID, motorRight, 0, vrep.simx_opmode_oneshot_wait);
 pause(1)
 [rtn,neoEndPos] = vrep.simxGetObjectPosition(clientID, neoHandle0, origoHandle, vrep.simx_opmode_oneshot_wait);
-neoMoved = neoEndPos - neoStartPos % displays the movement
+neoMoved = neoEndPos - neoStartPos; % displays the movement
 
 %% Get sick data
 [err, sick] = vrep.simxGetObjectHandle(0,'SICK_S300_fast#0', vrep.simx_opmode_oneshot_wait);
