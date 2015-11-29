24ce44bd32e7114e66af1883cae5f642fad62c2a
Erno Horvath <horverno@gmail.com>
2015-11-29 09:31:28 +0100

    [unstable]

--- VrepMatlabInteroperation/VrepMatlabSlam/slamGui.m
+++ VrepMatlabInteroperation/VrepMatlabSlam/slamGui.m
@@ -14,8 +14,12 @@ StpButton = uicontrol('Style','pushbutton','String','Stop', 'Position',[315,130,
 RstButton = uicontrol('Style','pushbutton','String','Reset','Position',[315,100,70,25],'Callback',{@RstButton_Callback});
 TxtOri = uicontrol('Style','edit','String',sprintf('\n'),   'Position',[315, 70,70,25],'Max', 4);
 ExtButton = uicontrol('Style','pushbutton','String','Exit', 'Position',[315, 50,70,15],'Callback',{@ExtButton_Callback});
+ChkBoxWall = uicontrol('Style','checkbox','String','Wall',  'Position',[315, 25,15,15],'Callback',{@CheckBox_Callback});
+ChkBoxEmpt = uicontrol('Style','checkbox','String','Empty', 'Position',[330, 25,15,15],'Callback',{@CheckBox_Callback});
+ChkBoxPth1 = uicontrol('Style','checkbox','String','Path1', 'Position',[345, 25,15,15],'Callback',{@CheckBox_Callback});
+ChkBoxPth2 = uicontrol('Style','checkbox','String','Path2', 'Position',[360, 25,15,15],'Callback',{@CheckBox_Callback});
 
-global RobotPathLayer WallLayer laserScan xW yW neoPose p poseAndTimeUnitTest neoPos
+global RobotPath1Layer RobotPath2Layer WallLayer laserScan xW yW neoPose p poseAndTimeUnitTest neoPos calcPosex calcPosey dT chkLayer
 
 ha = axes('Units','Pixels','Position',[50,60,200,185]); 
 align([FwdButton,BckButton,LftButton,RghButton,StpButton,RstButton,TxtOri],'Center','None');
@@ -26,16 +30,18 @@ align([FwdButton,BckButton,LftButton,RghButton,StpButton,RstButton,TxtOri],'Cent
 mapSize = 640; % the size of the map
 mapZoom = 50;  % the zoom factor 
 WallLayer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
-%EmptyLayer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
-RobotPathLayer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
+RobotPath1Layer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
+RobotPath2Layer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
 neoPose = struct('x', 0, 'y', 0, 'theta', 0); % contains the position and orientation of neobotix
 laserScan = 0;
-myColorMap = ([autumn; ones(6,3); flipud(winter); zeros(1,3)]); % the color map for display
+myColorMap1 = flipud([autumn; ones(6,3); flipud(winter); zeros(1,3)]); % the color map for blue white red
+myColorMap2 = flipud([autumn; ones(6,3); ones(65,3); ]); % the color map for white red
 probailityConstant = 0.95; % the constant of probability
+chkLayer = [true(1) true(1) false(1) false(1)]; % Wall Empty Path1 Path2 layers
 
 %% Initialize the GUI.
 % Change units to normalized so components resize automatically.
-set([f,ha,FwdButton,BckButton,LftButton,RghButton,StpButton,RstButton,TxtOri,ExtButton],'Units','normalized');
+set([f,ha,FwdButton,BckButton,LftButton,RghButton,StpButton,RstButton,TxtOri,ExtButton,ChkBoxPth1,ChkBoxPth2,ChkBoxWall,ChkBoxEmpt],'Units','normalized');
 %Create a plot in the axes.
 image(WallLayer);
 % Assign the GUI a name to appear in the window title.
@@ -61,8 +67,11 @@ DrawAllLayer()
 SetWheelSpeed(0,0);
 currentTime = toc;
 poseAndTimeUnitTest = double([]);
+calcPosex = neoPos(1);
+calcPosey = neoPos(2);
 
-%% Push button callbacks
+
+%% Push button and checkbox callbacks
 
 function FwdButton_Callback(~,~) 
   SetWheelSpeedToTarget(2,2, 4);
@@ -85,9 +94,8 @@ function StpButton_Callback(~,~)
 end
 
 function RstButton_Callback(~,~)
-  WallLayer = zeros(mapSize,mapSize);
-  %EmptyLayer = zeros(mapSize,mapSize);
-  RobotPathLayer = zeros(mapSize,mapSize);
+  WallLayer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
+  RobotPath1Layer = ones(mapSize,mapSize) / 2; % 0.5 probability by default
   DrawAllLayer()
 end
 
@@ -95,6 +103,11 @@ function ExtButton_Callback(~,~)
   disconnectVREP(vrep, clientID);
   close(f);
 end
+
+function CheckBox_Callback(~,~)
+  DrawAllLayer()
+end
+
 
 %% Nested functions
 function SetWheelSpeed(left, right)
@@ -118,8 +131,15 @@ function SetWheelSpeedToTarget(left, right, targetAngle)
        prevPos(2) = posMotor;
        if mod(i,10) == 0
            GetPose();
+           prevTime = currentTime;
            currentTime = toc;
+           dT = currentTime - prevTime;
+           calcPosex = calcPosex + left * 5 *(cos(neoPose.theta) * dT*1000);
+           calcPosey = calcPosey + left * 5 *(sin(neoPose.theta) * dT*1000);
+           [tx, ty] = ConvertXYToLayer(calcPosex, calcPosey);
+           WallLayer(tx+0, ty) = 0;
            poseAndTimeUnitTest = [poseAndTimeUnitTest; double(neoPose.x), double(neoPose.y),neoPose.theta, double(neoPos(1)), double(neoPos(2)), currentTime];
+           set(TxtOri, 'String', sprintf('x:%d %d\ny:%d %d\ntheta:%.3f\nTime:%.3f', neoPose.x, tx, neoPose.y, ty, neoPose.theta, currentTime));
        end
        if prevPos(1) > 0 && prevPos(2) < 0 % if the wheel reaches the  
            turns = turns + 1;
@@ -129,22 +149,23 @@ function SetWheelSpeedToTarget(left, right, targetAngle)
   SetWheelSpeed(0, 0) % stop the robot
 end
 
+function [xMap, yMap] = ConvertXYToLayer(xReal, yReal)
+  xMap = int64((xReal+11)*mapZoom); % convert neoPos to matrix element
+  yMap = int64((yReal+1)*mapZoom); % convert neoPos to matrix element
+end
+  
 function GetPose()
   [~,neoPos] = vrep.simxGetObjectPosition(clientID, sickHandle, origoHandle, vrep.simx_opmode_oneshot_wait);
   [~,neoOri] = vrep.simxGetObjectOrientation(clientID, neoHandle0, origoHandle, vrep.simx_opmode_oneshot_wait);
-  x = int64((neoPos(1)+11)*mapZoom); % convert neoPos to matrix element
-  y = int64((neoPos(2)+1)*mapZoom); % convert neoPos to matrix element
   % convert neoOrientaion 
   if neoOri(1) < 0
     neoOri(2) = neoOri(2) * - 1 + pi/2; 
   else
     neoOri(2) = neoOri(2) + pi + pi/2;
   end
-  neoPose.x = x;
-  neoPose.y = y;
+  [neoPose.x, neoPose.y] = ConvertXYToLayer(neoPos(1), neoPos(2));
   neoPose.theta = neoOri(2);
   currentTime = toc;
-  set(TxtOri, 'String', sprintf('x:%d\ny:%d\ntheta:%.3f\nTime:%.3f', x, y, neoPose.theta, currentTime));
 end
 
 function GetLaserScannerData()
@@ -168,10 +189,10 @@ function AddRobotToLayer()
       xD = neoPose.x + int64(n*cos(neoPose.theta));
       yD = neoPose.y + int64(n*sin(neoPose.theta));
       if (xD < mapSize && yD < mapSize && xD > 0 && yD > 0) % if they fit into the matrix
-        RobotPathLayer(xD, yD) = 60;
+        WallLayer(xD, yD) = 0.9;
       end
   end
-  RobotPathLayer(neoPose.x,neoPose.y) = 100;
+  %RobotPath1Layer(neoPose.x,neoPose.y) = 100;
 end
 
 function AddWallToLayer()
@@ -235,14 +256,27 @@ function points = CalcLine(x1, y1, x2, y2) % Calculates the laser beam lines (em
 end
 
 function DrawAllLayer()
-    AddWallToLayer()      
-    %AddRobotToLayer()  
-    AddEmptyToLayer()
-    colormap(myColorMap)
+    % Wall Empty Path1 Path2 layers
+    chkLayer(1) = get(ChkBoxWall,'Value');
+    chkLayer(2) = get(ChkBoxEmpt,'Value');
+    chkLayer(3) = get(ChkBoxPth1,'Value');
+    chkLayer(4) = get(ChkBoxPth2,'Value');    
+    if chkLayer(1)
+        AddWallToLayer()
+    end
+    if chkLayer(2)
+        AddEmptyToLayer()
+        colormap(myColorMap1)        
+    else
+        colormap(myColorMap2)  
+    end
+    if chkLayer(3)
+        AddRobotToLayer()  
+    end
     image(WallLayer,'CDataMapping','scaled');
-    h = zoom;
-    set(h,'Motion','both','Enable','on');
+    h = zoom; % zoom by default
     caxis([0,1])
+    set(h,'Motion','both','Enable','on'); % zoom by default
     colorbar
 
 end
