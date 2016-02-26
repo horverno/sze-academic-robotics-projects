7aae969e0258f27af187ea3c721864938efeccd9
Erno Horvath <horverno@gmail.com>
2016-02-26 17:40:01 +0100

    Map building added

---
+++ VrepMatlabInteroperation/VrepMatlabTest/MapBuildingTest/README.md
@@ -0,0 +1,10 @@
+# Map building
+Some useful files to work with.
+
+## Demo images
+
+![alt tag](http://www.sze.hu/~herno/robotics/map01measured.png)
+
+![alt tag](http://www.sze.hu/~herno/robotics/map01vrep.png)
+
+![alt tag](http://www.sze.hu/~herno/robotics/map01zoom.png)
---
+++ VrepMatlabInteroperation/VrepMatlabTest/MapBuildingTest/mapDisplay.m
@@ -0,0 +1,10 @@
+if(exist('map01a','var') == 0)
+    load('map01a.mat');
+end
+myColorMap = flipud([autumn; ones(6,3); flipud(winter); zeros(1,3)]); % the color map for blue white red
+colormap(gray)
+image(w6,'CDataMapping','scaled');
+h = zoom; % zoom by default
+caxis([0,1])
+set(h,'Motion','both','Enable','on'); % zoom by default
+colorbar
