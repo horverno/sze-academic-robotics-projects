2e1f98fb55493017dc83ae1bb67522d1da0e2852
Erno Horvath <horverno@gmail.com>
2016-06-23 17:37:06 +0200

    Coverage path planning algorithm (CPP) updated
    - Binary morphology-based blob detection
    - Maps added
    Next step: after blob recognition find the critical points which
    decomposes the map into sub-poygons

--- RobotGridCoveragePathPlanning/CoveragePathPlanningBoustrophedon.m
+++ RobotGridCoveragePathPlanning/CoveragePathPlanningBoustrophedon.m
@@ -22,16 +22,46 @@ mapUnderTest = map;
 %% Display map
 fig1 = figure('Name', 'Map');
 figure(fig1);
-image(mapUnderTest);
+image(uint8(~mapUnderTest) ); % + uint8(bwmorph(~mapUnderTest,'skel',Inf))
 h = zoom; % zoom by default
 set(h,'Motion','both','Enable','on'); % zoom by default
 colormap(myColorMap);
+colorbar();
 set(fig1,'Color',[1 1 1], 'units','pixels','outerposition',[0 0 800 800])
 movegui(fig1,'center')
-%measurements = regionprops(mapUnderTest, 'orientation');
-%angle = measurements(1).Orientation;
-%message = sprintf('The angle is %.3f degrees\n', angle);
-%uiwait(msgbox(message));
+
+
+%% Create blobs
+[blobs, ~, blobNumber, adjacencyMat] = bwboundaries(~mapUnderTest);
+% Test: visulalize blobs
+hold on
+% Boundary is the parent of a hole if the k-th column
+% of the adjacency matrix A contains a non-zero element
+% there shold be only 1 parent, because the map is coherent
+if (nnz(adjacencyMat(:,1)) > 0)
+    boundary = blobs{1};
+    plot(boundary(:,2), boundary(:,1), 'y', 'LineWidth', 4);
+    % Loop through the children of boundary k
+    for i = find(adjacencyMat(:,1))'
+        boundary = blobs{i};
+        plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 4);
+    end
+end
+
+% todo: after blob recognition find the critical points which decomposes the map into sub-poygons
+
+%%
+% measurements = regionprops(mapUnderTest, 'orientation');
+% angle = measurements(1).Orientation;
+% message = sprintf('The angle is %.3f degrees\n', angle);
+% uiwait(msgbox(message));
+% hold off
+% contour = bwtraceboundary(logical(mapUnderTest),[163 37],'W',8,Inf,'counterclockwise');
+% hold on;
+% plot(contour(:,2),contour(:,1),'g','LineWidth',2);
+% BW3 = bwmorph(~mapUnderTest,'skel',Inf);
+% figure
+% imshow(BW3)
 
 %% Display debug info
 % fig2 = figure('Name', 'Covered area', 'Position',[20,500,450,285]);
